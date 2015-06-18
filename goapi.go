e77ee4cc69dab309a0b676b8894d095d9d8a856c
Jeff Pizano <m4dem4n@gmail.com>
2015-06-18 01:41:48 -0700

    Added endpoints '/md5' and '/eightball'

--- goapi.go
+++ goapi.go
@@ -1,21 +1,69 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
+
+var eightBallResponse []string = []string{
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt",
+	"Yes definitely",
+	"You may rely on it",
+	"As I see it yes",
+	"Most likely",
+	"Outlook good",
+	"Yes",
+	"Signs point to yes",
+	"Reply hazy try again",
+	"Ask again later",
+	"Better not tell you now",
+	"Cannot predict now",
+	"Concentrate and ask again",
+	"Don't count on it",
+	"My reply is no",
+	"My sources say no",
+	"Outlook not so good",
+	"Very doubtful",
+}
 
 func homeHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Welcome to my Go API.")
 }
 
-func authHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "Authentication endpoint")
+func md5Handler(w http.ResponseWriter, req *http.Request) {
+	parms := req.URL.Query()
+	_, ok := parms["v"]
+
+	if ok {
+		data := []byte(req.URL.Query()["v"][0])
+		fmt.Fprintf(w, "%x", md5.Sum(data))
+	} else {
+		fmt.Fprintln(w, "Add query string '?v=hashThis' to url.")
+	}
+}
+
+func sha1Handler(w http.ResponseWriter, req *http.Request) {
+	//data := []byte(req.URL.Query()["v"][0])
+
+}
+
+func eightBallHandler(w http.ResponseWriter, req *http.Request) {
+	l := len(eightBallResponse)
+	rand.Seed(time.Now().UnixNano())
+	fmt.Fprintln(w, eightBallResponse[rand.Intn(l)])
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/md5", md5Handler)
+	http.HandleFunc("/eightball", eightBallHandler)
+
 	log.Println("Attempting to listen on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
