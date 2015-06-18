681d15f7fcae79c86fe7232bd5bdf9ee8872c235
Jeff Pizano <m4dem4n@gmail.com>
2015-06-17 18:35:07 -0700

    First commit.

---
+++ goapi.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Welcome to my Go API.")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	err := http.ListenAndServe("localhost:80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
