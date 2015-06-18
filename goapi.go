e9bcaae0b3bfa87ad9e26f92a5bcc3bb41201a4f
Jeff Pizano <m4dem4n@gmail.com>
2015-06-18 00:18:50 -0700

    Try testing with working code...

--- goapi.go
+++ goapi.go
@@ -10,9 +10,13 @@ func homeHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Welcome to my Go API.")
 }
 
+func authHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Authentication endpoint")
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
-	log.Println("Attempting to listen on port 8080", port)
+	log.Println("Attempting to listen on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
