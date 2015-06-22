9036825bfb2ba58fa99281f0304f616d7e132cb7
Jeff Pizano <m4dem4n@gmail.com>
2015-06-22 15:49:35 -0700

    Color update

--- igoapi.sh
+++ igoapi.sh
@@ -7,10 +7,6 @@ APP_PATH="/home/ec2-user/go/src/github.com/m4dem4n/goapi"
 
 TMP_FILE="/tmp/status_$APP_FILENAME"
 
-LGREEN="\[\033[1;32m\]"
-COLOR_RED="\\033[1;31m"
-LGREY="\[\033[0;39m\]"
-
 S_STATUS[0]="Running"
 S_STATUS[1]="Not Running"
 S_STATUS[2]="Stopped incorrectly"
@@ -24,9 +20,9 @@ start() {
 	if [ $STATUS -ne 0 ]; then
 		nohup $APP_PATH/$APP_FILENAME > $APP_PATH/$APP_FILENAME.out 2> $APP_PATH/$APP_FILENAME.err < /dev/null &
 		echo $! > $APP_PID
-		echo -e "[$LGREENSuccess$LGREY]"
+		echo -e "[\\033[1;32m]Success\\033[0m]]"
 	else
-		echo -e "[\\033[1;31mFailure\\033[0;39m]"
+		echo -e "[\\033[1;31m]Failure\\033[0;39m]]"
 	fi
 }
 
